392eece460d0424da20dddd6c760e3f7bb69121f
VON1220 <57125583+VON1220@users.noreply.github.com>
2022-04-21 12:22:26 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "485e2170-807c-408e-c470-c8c2465b796a",
                         "alterId": 0
                     }
                 ],
